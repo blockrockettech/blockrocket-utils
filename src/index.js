54d2341b2d71d8d56c74619d580a55bd4a467a60
vince0656 <vince0656@googlemail.com>
2019-10-02 15:09:44 +0100

    Added network splitter to the utils package

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const networkSplitter = (network, {ropsten, rinkeby, mainnet, local}) => {
+    switch (network) {
+        case 1:
+        case '1':
+        case 'mainnet':
+            return mainnet;
+        case 3:
+        case '3':
+        case 'ropsten':
+            return ropsten;
+        case 4:
+        case '4':
+        case 'rinkeby':
+            return rinkeby;
+        case 5777:
+        case '5777':
+        case 'local':
+            // This may change if a clean deploy of KODA locally is not done
+            return local;
+        default:
+            throw new Error(`Unknown network ID ${network}`);
+    }
+};
+
+const getNetwork = (network) => {
+    return networkSplitter(network, {
+        mainnet: 'mainnet',
+        ropsten: 'ropsten',
+        rinkeby: 'rinkeby',
+        local: 'local'
+    });
+};
+
+module.exports = {
+    getNetwork,
+    networkSplitter
+};
