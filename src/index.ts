92fd64b20c2eb1aa9ed6eeb29172d4f9636c4dd1
vince0656 <vince0656@googlemail.com>
2019-10-02 16:39:09 +0100

    Added missing types

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-export function networkSplitter(network, {ropsten, rinkeby, mainnet, local}) {
+interface NetworkNameMappings {ropsten: string, rinkeby: string, mainnet: string, local: string}
+
+export function networkSplitter(network: string, {ropsten, rinkeby, mainnet, local}: NetworkNameMappings): string {
     switch (network) {
-        case 1:
         case '1':
         case 'mainnet':
             return mainnet;
-        case 3:
         case '3':
         case 'ropsten':
             return ropsten;
-        case 4:
         case '4':
         case 'rinkeby':
             return rinkeby;
-        case 5777:
         case '5777':
         case 'local':
             // This may change if a clean deploy of KODA locally is not done
@@ -22,7 +20,7 @@ export function networkSplitter(network, {ropsten, rinkeby, mainnet, local}) {
     }
 }
 
-export function getNetwork(network) {
+export function getNetwork(network: string): string {
     return networkSplitter(network, {
         mainnet: 'mainnet',
         ropsten: 'ropsten',
