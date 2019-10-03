9a06edc7d2ee854f687466900e611b99682eafdc
vince0656 <vince0656@googlemail.com>
2019-10-03 10:11:32 +0100

    Added typescript def file

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+export function getNetworkName(network: any): any;
+
+export function getRpcConnectionUri(network: any, infuraKey: any): any;
+
+export function networkSplitter(network: any, {ropsten, rinkeby, mainnet, local, sokol, poa, xdai}: any): any;
+
